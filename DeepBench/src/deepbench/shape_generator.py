cf10c8904dfac85dc760d0e5a33f1ef3350af651
voetberg <magpie127@gmail.com>
2022-03-17 13:33:49 -0500

    empty functions and init test

---
+++ DeepBench/src/deepbench/shape_generator.py
@@ -0,0 +1,42 @@
+class ShapeGenerator:
+    def __init__(self):
+        pass
+
+    def create_meshgrid(self):
+        pass
+
+    @staticmethod
+    def resize():
+        pass
+
+    @staticmethod
+    def _convert_path_to_image():
+        pass
+
+    @staticmethod
+    def create_rectangle():
+        pass
+
+    @staticmethod
+    def create_polygon():
+        pass
+
+    @staticmethod
+    def create_circle():
+        pass
+
+    @staticmethod
+    def create_arc():
+        pass
+
+    @staticmethod
+    def create_line():
+        pass
+
+    @staticmethod
+    def create_ellipse():
+        pass
+
+    @staticmethod
+    def create_empty_shape():
+        pass
---
+++ DeepBench/tests/shape_generator_test.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from src.deepbench.shape_generator import ShapeGenerator
+
+
+class TestShapeGenerator(TestCase):
+    def test_define(self):
+        pass
