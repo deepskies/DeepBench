5546f3edc27a14002bacbe2326e6861dc6e64973
AeRabelais <pantagruelspendulum@protonmail.com>
2022-03-19 13:02:13 -0500

    quick fix

---
+++ src/deepbench/astro_object/astro_object.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractclassmethod
+"""
+class AstroObject(ABC):
+
+    def __init__(self, image_dim, position, radius, amplitude, noise, gaussian_blur):
+
+        self.image = np.zeros((image_dim, image_dim))
+        self.pos = position
+        self.rad = radius
+        self.amp = amplitude
+        self.noise = noise
+        self.blur = gaussian_blur
+
+    def create_object(self):
+
+        raise NotImplementedError();
+
+    def create_psf(self):
+
+    def create_noise(self):
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
---
+++ src/deepbench/astro_object/star_object.py
@@ -0,0 +1,12 @@
+"""
+class StarObject(AstroObject):
+
+    classname = 'Star'
+
+    def __init__(self, img_dim, center, radius, amplitude, noise, gaussian_blur):
+
+        super.__init__(Star, self, position=center, radius=radius, amplitude=amplitude,
+                       image_noise=pois_noise, gaussian_blur=gaussianBlur, image_dim=img_dim)
+
+
+"""
