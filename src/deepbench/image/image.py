39f0419d3d3c0d73e1d85a625924b69a5607a132
voetberg <magpie127@gmail.com>
2022-03-31 15:45:19 -0500

    Empty Classes

---
+++ src/deepbench/image/image.py
@@ -0,0 +1,6 @@
+class Image:
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
---
+++ src/deepbench/image/sky_image.py
@@ -0,0 +1,6 @@
+from src.deepbench.image.image import Image
+
+
+class SkyImage(Image):
+    def __init__(self):
+        pass
