e0cbd6f5bebe6f7c84cf3af2bc9c2c68b891ba2d
AeRabelais <pantagruelspendulum@protonmail.com>
2022-04-08 19:17:08 -0500

    test

--- src/deepbench/astro_object/astro_object.py
+++ src/deepbench/astro_object/astro_object.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractclassmethod
-"""
+
+# from scipy import ndimage
+import numpy.random as rand
+import numpy as np
+
+
 class AstroObject(ABC):
-
     def __init__(self, image_dim, position, radius, amplitude, noise, gaussian_blur):
-
+        """
+        Comment Container.
+        """
         self.image = np.zeros((image_dim, image_dim))
         self.pos = position
         self.rad = radius
@@ -12,27 +18,26 @@ class AstroObject(ABC):
         self.blur = gaussian_blur
 
     def create_object(self):
+        """
+        Function Description.
 
-        raise NotImplementedError();
+        Args:
+            Argument Container.
+        Returns:
+            Output Container.
+        Raises:
+            Error Container.
+        """
+        raise NotImplementedError()
 
     def create_psf(self):
+        """
+        Comment Container.
+        """
+        # self.image = ndimage.gaussian_filter(self.image, sigma=self.image_blur)
 
     def create_noise(self):
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
+        """
+        Comment Container.
+        """
+        self.image += rand.poisson(lam=self.image_noise, size=self.image.shape)
