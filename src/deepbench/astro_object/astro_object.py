dc66e0a2af84fd326270b53a7740d19de4145a19
AeRabelais <pantagruelspendulum@protonmail.com>
2022-04-08 22:50:06 -0500

    finalized first iteration of the astro_object file

--- src/deepbench/astro_object/astro_object.py
+++ src/deepbench/astro_object/astro_object.py
@@ -1,14 +1,30 @@
 from abc import ABC, abstractclassmethod, abstractmethod
-
+from typing import Union, List
 from scipy import ndimage
 import numpy.random as rand
 import numpy as np
 
 
 class AstroObject(ABC):
-    def __init__(self, image_dimensions, radius, amplitude, noise_level):
+    def __init__(
+        self,
+        image_dimensions: Union[int, float, List[int], List[float]],
+        radius: Union[int, float],
+        amplitude: Union[int, float],
+        noise: Union[float, List[float]],
+    ) -> None:
         """
-        Comment Container.
+        The initialization function for the Abstract AstroObject class.
+
+        Args:
+            image_dimensions (Union[int, float, List[int], List[float]]): The dimension(s) of the object to be produced.
+            radius (Union[int, float]): The radius of the object to be produced.
+            amplitude (Union[int, float]): The amplitude of the object to be produced.
+            noise_level (Union[float, list[float]]): The Poisson noise level to be applied to the object.
+
+        Examples:
+
+            >>> example_obj = AstroObject(image_dimensions=28, radius=5, amplitude=3, noise_level=0.7)
         """
 
         #   TODO:
@@ -17,39 +33,68 @@ class AstroObject(ABC):
         self._image = np.zeros((image_dimensions, image_dimensions))
         self._radius = radius
         self._amplitude = amplitude
-        self._noise_level = noise_level
+        self._noise_level = noise
 
     @abstractmethod
     def create_object(self):
         """
-        Function Description.
+        Creates the astronomical object.
 
-        :param container: Container Description.
-        :type container: Container Type.
-        :raise Error: Error Description.
-        :return: Output Description.
-        :rtype: list[str]
+        Raises:
+            NotImplementedError: If the child class does not implement the method.
         """
         raise NotImplementedError()
 
     @staticmethod
-    def create_psf(self, gaussian_blur=0.7):
-        """ """
+    def create_psf(self, gaussian_blur=0.7) -> np.ndarray:
+        """
+        Creates the Point Spread Function to append to the object.
+
+        Args:
+            gaussian_blur (float): The level of gaussian blur to be applied.
+
+        Returns:
+            ndarray: The PSF as an array the same shape as the input.
+
+        Examples:
+            >>> example_obj.create_psf(gaussian_blur=1.2)
+            >>> example_obj.create_psf()
+        """
         return ndimage.gaussian_filter(self._image.shape, sigma=gaussian_blur)
 
     @staticmethod
-    def create_noise(self, seed=42):
-        """ """
+    def create_noise(self, seed=42) -> np.ndarray:
+        """
+        Creates the Poisson noise added to the object.
+
+        Args:
+            seed (int): The random initialization seed used for reproducibility.
+
+        Returns:
+            ndarray: A random sample drawn from a Poisson distribution.
+
+        Examples:
+            >>> example_obj.create_noise()
+            >>> example_obj.create_noise(seed=5)
+        """
 
         rs = rand.RandomState(seed)
-        return rs.poisson(self._noise_level, size=self.image.shape)
+        return rs.poisson(self._noise_level, size=self._image.shape)
 
     @staticmethod
-    def create_meshgrid(self):
-        """ """
+    def create_meshgrid(self) -> np.ndarray:
+        """
+        Creates a meshgrid for the object.
+
+        Returns:
+            ndarray: A vector of coordinates.
+
+        Examples:
+            >>> example_obj.create_meshgrid()
+        """
         meshgrid = np.meshgrid(
-            np.arange(np.shape(self.image.shape)[0]),
-            np.arange(np.shape(self.image.shape)[1]),
+            np.arange(np.shape(self._image.shape)[0]),
+            np.arange(np.shape(self._image.shape)[1]),
         )
 
         return meshgrid
