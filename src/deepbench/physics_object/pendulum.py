dcc69b2a15de865e6b0a62bf10623e1961171d0c
beckynevin <beckynevin@gmail.com>
2023-05-05 09:25:43 -0600

    pasted in code

--- src/deepbench/physics_object/pendulum.py
+++ src/deepbench/physics_object/pendulum.py
@@ -1,23 +1,23 @@
-import numpy as np
 from src.deepbench.physics_object.physics_object import PhysicsObject
-from typing import Optional, List, Union
-from matplotlib.animation import FuncAnimation
-
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from typing import Union, List, Optional
+
 
 class Pendulum(PhysicsObject):
-    def __init__(self, pendulum_arm_length: float,
+    def __init__(self,
+                 pendulum_arm_length: float,
                  starting_angle_radians: float,
-                 #TODO change noise to a dictionary
-                 noise_std_percent: dict = {},
+                 #noise_std_percent: dict = {},
+                 noise_std_percent: Union[float, List[float]] = 0.0,
+                 calculation_type: str = "x position",
                  acceleration_due_to_gravity: Optional[float] = None,
                  big_G_newton: Optional[float] = None,
                  phi_planet: Optional[float] = None,
                  mass_pendulum_bob: Optional[float] = 10.0,
-                 coefficient_friction: Optional[float] = 0.0):
-
-        super().__init__(noise_level=noise_std_percent)
-
+                 coefficient_friction: Optional[float] = 0.0
+                 ):
         """
         The initialization function for the Pendulum class.
 
@@ -49,9 +49,13 @@ class Pendulum(PhysicsObject):
             >>> pendulum_obj = Pendulum(pendulum_arm_length=10.,
                                         starting_angle_radians=np.pi/4,
                                         acceleration_due_to_gravity=9.8,
-                                        noise_standard_percent = 0.1
+                                        noise_standard_percent = 0.1,
+                                        calculation_type = 'x position'
                                         )
         """
+        super().__init__(
+            noise_level=noise_std_percent,
+        )
 
         self.pendulum_arm_length = pendulum_arm_length
         self.starting_angle_radians = starting_angle_radians
@@ -79,16 +83,50 @@ class Pendulum(PhysicsObject):
         # TODO verify the requested noise parameters are variables you can use
 
         for key, item in noise_std_percent.items():
-            assert key in [key for key in self.dict.keys()] # key is a variablable in the class
+            assert key in [key for key in self.dict.keys()]
+            # key is a variable in the class
             assert type(item) in [np.array, float]
 
     def create_noise(self, seed: int = 42, n_steps: int | tuple[int, int] = 10) -> np.array:
-        return NotImplemented
+        # Make a list of parameters to add noise to
+        # Here we add noise not just on the final measurement
+        # but via adding it to each parameter and propagating
+        # through to the final measurement (ie x position)
+        parameter_list = [self.pendulum_arm_length,
+                          self.starting_angle_radians,
+                          self.acceleration_due_to_gravity]
+        # Define the standard deviation of noise for each parameter
+        if type(self.noise) is float:  # then its one number
+            std_noise = [self.noise * p for p in parameter_list]
+        else:
+            for i, p in enumerate(parameter_list):
+                std_noise = [self.noise[i] * p for p in parameter_list]
+        # Add noise to global parameters
+        self.pendulum_arm_length_noisy = np.random.normal(
+            loc=self.pendulum_arm_length,
+            scale=std_noise[0]
+        )
+        self.starting_angle_radians = np.random.normal(
+            loc=self.starting_angle_radians,
+            scale=std_noise[1]
+        )
+        self.acceleration_due_to_gravity = np.random.normal(
+            loc=self.acceleration_due_to_gravity,
+            scale=std_noise[2]
+        )
 
-    def destory_noise(self):
-        pass
+    def destroy_noise(self):
+        # Re-modify the global parameters to
+        # have the original value
+        self.pendulum_arm_length = \
+            self.initial_parameters['pendulum_arm_length']
+        self.starting_angle_radians = \
+            self.initial_parameters['starting_angle_radians']
+        self.acceleration_due_to_gravity = \
+            self.initial_parameters['acceleration_due_to_gravity']
+        return
 
-    def create_object(self, time:np.array, noiseless:bool=False, seed:int=42):
+    def create_object(self, time: np.array, noiseless: bool = False, seed: int = 42):
 
         self.create_noise(seed=seed, n_steps=time.shape)
         if noiseless:
@@ -99,11 +137,16 @@ class Pendulum(PhysicsObject):
         self.destroy_noise()
         return pendulum
 
+    def simulate_pendulum_dynamics(self, time: Union[float, np.array[float]]):
+        assert len(time) is not None, "Must enter a time"
+        time = np.asarray(time)
+        theta_time = self.starting_angle_radians * \
+            np.cos(np.sqrt(self.acceleration_due_to_gravity /
+                           self.pendulum_arm_length))
+        x = self.pendulum_arm_length * np.sin(theta_time * time)
+        return x
 
-    def simulate_pendulum_dynamics(self, time:Union[float, np.array[float]]):
-        return NotImplemented
-
-    def displayObject(self, time:Union[float, np.array[float]]):
+    def displayObject(self, time: Union[float, np.array[float]]):
         plt.clf()
         plt.scatter(time, self.create_object(time),
                     label='noisy')
