efaa1ffd4cd70349125459c0da083d7b6b702f28
voetberg <magpie127@gmail.com>
2023-06-05 11:20:04 -0500

    test outline and module logic

--- src/deepbench/collection/collection.py
+++ src/deepbench/collection/collection.py
@@ -1,3 +1,52 @@
+import sys
+
+from src.deepbench.image.sky_image import SkyImage
+from src.deepbench.image.shape_image import ShapeImage
+from src.deepbench.physics_object.physics_object import PhysicsObject
+
+
 class Collection:
-    def __init__(self):
-        pass
+    def __init__(self, object_config: dict):
+
+        self.object_type = object_config["object_type"]
+        self.object_name = object_config["object_name"]
+
+        self.total_objects = object_config["total_objects"]
+        self.included_params = object_config["object_parameters"]
+
+        object_parent_class = {
+            "sky": SkyImage,
+            "shape": ShapeImage,
+            "physics": PhysicsObject,
+        }[self.object_type]
+
+        possible_engines = {
+            engine.__name__: engine
+            for engine in sys.modules
+            if issubclass(engine, object_parent_class)
+        }
+
+        self.object_engine = possible_engines[self.object_name]
+
+        self.objects = {}
+        self.n_objects = 0
+
+    def add_object(self):
+        random_seed = ""
+
+        if self.object_type in ["sky", "shape"]:
+            object = self.object_engine.combine_objects()
+
+        elif self.object_type == "physics":
+            object = self.object_engine.create_object()
+
+        self.objects[self.n_objects] = object
+
+        self.object_params[self.n_objects] = {param for param in ""}
+
+        self.object_params[self.n_objects]["seed"] = random_seed
+        self.n_object += 1
+
+    def __call__(self):
+        for _ in self.total_objects:
+            self.add_object()
--- tests/collection_test.py
+++ tests/collection_test.py
@@ -1,5 +1,65 @@
 import pytest
+import os
+import yaml
+from src.deepbench.collection.collection import Collection
+
+
+@pytest.fixture()
+def default_physics():
+    return yaml.safe_load(
+        f"{os.path(__file__)}/../src/deepbench/settings/default_physics_object.yaml"
+    )
+
+
+@pytest.fixture()
+def default_sky():
+    return yaml.safe_load(
+        f"{os.path(__file__)}/../src/deepbench/settings/default_sky_object.yaml"
+    )
+
+
+@pytest.fixture()
+def default_shape():
+    return yaml.safe_load(
+        f"{os.path(__file__)}/../src/deepbench/settings/default_shapes.yaml"
+    )
 
 
 def test_default_init():
+    pass
+
+
+def test_add_single_item():
+    pass
+
+
+def test_format_params():
+    pass
+
+
+def test_add_seed():
+    pass
+
+
+def test_find_modules():
+    pass
+
+
+def test_make_image():
+    pass
+
+
+def test_make_shape():
+    pass
+
+
+def test_make_physics_obj():
+    pass
+
+
+def test_make_multiple():
+    pass
+
+
+def test_missing_param():
     pass
