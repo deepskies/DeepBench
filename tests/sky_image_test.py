faf19b62d04950c854de680184ffcba2c13f0ab0
voetberg <magpie127@gmail.com>
2023-06-06 16:10:38 -0500

    Updated to the other version of dev

--- tests/sky_image_test.py
+++ tests/sky_image_test.py
@@ -1,107 +1,125 @@
-from unittest import TestCase
+import pytest
 from src.deepbench.image.sky_image import SkyImage
 
 
-class TestSkyImage(TestCase):
-    def test_init(self):
-        test_sky = SkyImage([{}], (14, 14))
+@pytest.fixture()
+def star():
+    return (
+        "star",
+        {"center_x": 14, "center_y": 14, "alpha": 1.0},
+        {"noise": 0, "radius": 1.0, "amplitude": 1.0},
+    )
 
-        self.assertIsNone(test_sky.image)
-        self.assertEqual([{}], test_sky.objects)
-        self.assertEqual((14, 14), test_sky.image_shape)
 
-    def test_1dim_size(self):
-        with self.assertRaises(AssertionError):
-            im_shape = (12,)
-            SkyImage([{}], im_shape)
+def test_init():
+    test_sky = SkyImage((14, 14))
+    assert (14, 14) == test_sky.image_shape
 
-    def test_0dim_size(self):
-        with self.assertRaises(AssertionError):
-            im_shape = ()
-            SkyImage([{}], im_shape)
 
-    def test_3dim_size(self):
+def test_1dim_size():
+    with pytest.raises(AssertionError):
+        im_shape = (12,)
+        SkyImage(im_shape)
 
-        im_shape = (14, 14, 3)
-        test_sky = SkyImage([{}], im_shape)
 
-        self.assertIsNone(test_sky.image)
-        self.assertEqual([{}], test_sky.objects)
-        self.assertEqual(im_shape, test_sky.image_shape)
+def test_0dim_size(self):
+    with self.assertRaises(AssertionError):
+        im_shape = ()
+        SkyImage(im_shape)
 
-    def test_combine_one_image(self):
-        object_params = [{"object_type": "test_object"}]
+
+def test_3dim_size():
+    im_shape = (14, 14, 3)
+    test_sky = SkyImage(im_shape)
+
+    assert im_shape == test_sky.image_shape
+
+
+def test_combine_one_image(star):
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(image_shape)
+
+    # Not testing that they're the right ones, only that they're made
+    one_image_sky.combine_objects(star)
+
+    assert image_shape == one_image_sky.image.shape
+
+
+def test_combine_2_images(star):
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(image_shape)
+
+    multiple_objects = [star[0], star[0]], [star[1], star[1]], [star[2], star[2]]
+    # Not testing that they're the right ones, only that they're made
+    one_image_sky.combine_objects(multiple_objects)
+
+    assert image_shape == one_image_sky.image.shape
+
+
+def test_combine_no_images():
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(image_shape)
+
+    assert 0 == one_image_sky.image.sum()
+
+
+def test_generate_gaussian_noise(star):
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(
+        image_shape, object_noise_level=0.1, object_noise_type="guassian"
+    )
+    one_image_sky.combine_objects(star)
+
+    assert image_shape == one_image_sky.image.shape
+    assert one_image_sky.image.any() != one_image_sky._generate_astro_object(star)
+
+
+def test_generate_poisson_noise(star):
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(image_shape)
+    one_image_sky.combine_objects(
+        star, object_noise_level=0.1, object_noise_type="poisson"
+    )
+
+    assert image_shape == one_image_sky.image.shape
+    assert one_image_sky.image.any != one_image_sky._generate_astro_object(star)
+
+
+def test_add_fake_noise():
+    with pytest.raises(NotImplementedError):
+
+        fake_noise = "Not A Noise Type"
         image_shape = (14, 14)
-        one_image_sky = SkyImage(object_params, image_shape)
-
-        # Not testing that they're the right ones, only that they're made
+        one_image_sky = SkyImage(image_shape, object_noise_type=fake_noise)
         one_image_sky.combine_objects()
 
-        self.assertEqual(image_shape, one_image_sky.image.shape)
 
-    def test_combine_2_images(self):
-        object_params = [{"object_type": "test_object"}, {"object_type": "test_object"}]
-        image_shape = (14, 14)
-        one_image_sky = SkyImage(object_params, image_shape)
-
-        # Not testing that they're the right ones, only that they're made
-        one_image_sky.combine_objects()
-
-        self.assertEqual(image_shape, one_image_sky.image.shape)
-
-    def test_combine_no_images(self):
-        object_params = [{}]
-        image_shape = (14, 14)
-        one_image_sky = SkyImage(object_params, image_shape)
-
-        # Not testing that they're the right ones, only that they're made
-        with self.assertWarns(UserWarning):
-            one_image_sky.combine_objects()
-
-        self.assertEqual(image_shape, one_image_sky.image.shape)
-        self.assertEqual(0, one_image_sky.image.sum())
-
-    def test_generate_gaussian_noise(self):
-
-        object_params = [{"object_type": "test_object", "object_parameters": {}}]
+def test_image_not_made():
+    with pytest.raises(AssertionError):
 
         image_shape = (14, 14)
-        one_image_sky = SkyImage(object_params, image_shape)
-        one_image_sky.combine_objects()
-        one_image_sky.generate_noise("gaussian")
+        one_image_sky = SkyImage(image_shape)
+        one_image_sky.generate_noise()
 
-        self.assertIsNotNone(one_image_sky.image)
-        self.assertEqual(image_shape, one_image_sky.image.shape)
 
-    def test_generate_poisson_noise(self):
-
-        object_params = [{"object_type": "test_object", "object_parameters": {}}]
+def test_not_an_astro_object(star):
+    fake_object = "I wanted to write a funny joke here but my brain is gone"
+    with pytest.raises(AssertionError):
 
         image_shape = (14, 14)
-        one_image_sky = SkyImage(object_params, image_shape)
-        one_image_sky.combine_objects()
-        one_image_sky.generate_noise("poisson")
+        one_image_sky = SkyImage(image_shape)
+        one_image_sky.generate_noise(fake_object, star[1], star[2])
 
-        self.assertIsNotNone(one_image_sky.image)
-        self.assertEqual(image_shape, one_image_sky.image.shape)
 
-    def test_add_fake_noise(self):
-        with self.assertRaises(NotImplementedError):
+def test_make_all_astro_objects():
+    sky_objects = ["star", "galaxy", "spiral_galaxy"]
+    sky_params = [{"noise": 0, "radius": 1.0, "amplitude": 1.0}, {}, {}]
+    object_params = [
+        {"center_x": 14, "center_y": 14, "alpha": 1.0},
+        {"center_x": 14, "center_y": 14},
+        {"center_x": 14, "center_y": 14},
+    ]
 
-            object_params = [{"object_type": "test_object", "object_parameters": {}}]
-
-            image_shape = (14, 14)
-            one_image_sky = SkyImage(object_params, image_shape)
-            one_image_sky.combine_objects()
-            one_image_sky.generate_noise("Fake Noise")
-
-    def test_image_not_made(self):
-        with self.assertRaises(AssertionError):
-
-            object_params = [{"object_type": "test_object", "object_parameters": {}}]
-
-            image_shape = (14, 14)
-            one_image_sky = SkyImage(object_params, image_shape)
-
-            ##Go straight to noise instead of making objects first
-            one_image_sky.generate_noise("gaussian")
+    image_shape = (14, 14)
+    one_image_sky = SkyImage(image_shape)
+    one_image_sky.combine_objects(sky_objects, sky_params, object_params)
