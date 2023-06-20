0bb33c1116c177d384d0547f0ac14475ce08a784
voetberg <magpie127@gmail.com>
2023-06-20 13:53:49 -0500

    autodocs stage 1

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "DeepBench"
+copyright = "2023, Ashia Livaudais, M. Voetberg, Becky Nevin, Omari Paul, Brian Nord"
+author = "Ashia Livaudais, M. Voetberg, Becky Nevin, Omari Paul, Brian Nord"
+release = "0.1.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
